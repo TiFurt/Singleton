2a6449a3cacb98eb78455a36a1898b21388898b5
TFurtado <tfurtado@bocavox.com>
2023-09-20 21:56:16 -0300

    removing unnecessary import

--- src/test/java/projeto/software/fileReader/FileReaderTest.java
+++ src/test/java/projeto/software/fileReader/FileReaderTest.java
@@ -1,9 +1,8 @@
 package projeto.software.fileReader;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileReaderTest {
 
